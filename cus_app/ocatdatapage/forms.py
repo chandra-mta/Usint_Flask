b870182dc0a138ffc1d51f2b846020ec8da001d2
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2025-03-14 17:20:15 -0400

    format ocat_data into form data

--- cus_app/ocatdatapage/forms.py
+++ cus_app/ocatdatapage/forms.py
@@ -42,33 +42,33 @@ class GeneralParamForm(FlaskForm):
     targname = StringField("Target Name", validators=[DataRequired()])
 
     choice = ("ACIS-I", "ACIS-S", "HRC-I", "HRC-S")
-    instrument = SelectField("Instrument :", choices=[(x, x) for x in choice])
+    instrument = SelectField("Instrument", choices=[(x, x) for x in choice])
 
     choice = (None, "LETG", "HETG")
-    grating = SelectField("Grating :", choices=[(x, x) for x in choice])
+    grating = SelectField("Grating", choices=[(x, x) for x in choice])
 
     choice = ('GO', 'TOO', 'GTO', 'CAL', 'DDT', 'CAL_ER', 'ARCHIVE', 'CDFS', 'CLP')
-    type = SelectField("Type :", choices=[(x, x) for x in choice])
+    type = SelectField("Type", choices=[(x, x) for x in choice])
 
-    ra_hms = StringField("RA (HMS) :")
-    dec_dms = StringField("DEC (DMS) :")
+    ra_hms = StringField("RA (HMS)")
+    dec_dms = StringField("DEC (DMS)")
 
-    y_det_offset = StringField("Offset Y :")
-    z_det_offset = StringField("Offset Z :")
-    trans_offset = StringField("Z-Sim :")
-    focus_offset = StringField("Sim-Focus :")
+    y_det_offset = StringField("Offset Y")
+    z_det_offset = StringField("Offset Z")
+    trans_offset = StringField("Z-Sim")
+    focus_offset = StringField("Sim-Focus")
 
-    uninterrupt = SelectField("Uninterrupted Obs :", choices=_CHOICE_NNPY)
-    extended_src = SelectField("Extended SRC :", choices=_CHOICE_NY)
-    obj_flag = SelectField("Solar System Object :", choices=[(x, x) for x in ('NO', 'MT', 'SS')])
-    object = SelectField("Object :", choices=[(x, x) for x in (None, 'NEW','ASTEROID', 'COMET', 'EARTH', 'JUPITER', 'MARS','MOON', 'NEPTUNE', 'PLUTO', 'SATURN', 'URANUS', 'VENUS')])
-    photometry_flag = SelectField("Photometry :", choices=_CHOICE_NNY)
-    vmagnitude = StringField("V Mag :")
-    est_cnt_rate = StringField("Count Rate :")
-    forder_cnt_rate = StringField("1st Order Rate :")
+    uninterrupt = SelectField("Uninterrupted Obs", choices=_CHOICE_NNPY)
+    extended_src = SelectField("Extended SRC", choices=_CHOICE_NY)
+    obj_flag = SelectField("Solar System Object", choices=[(x, x) for x in ('NO', 'MT', 'SS')])
+    object = SelectField("Object", choices=[(x, x) for x in (None, 'NEW','ASTEROID', 'COMET', 'EARTH', 'JUPITER', 'MARS','MOON', 'NEPTUNE', 'PLUTO', 'SATURN', 'URANUS', 'VENUS')])
+    photometry_flag = SelectField("Photometry", choices=_CHOICE_NNY)
+    vmagnitude = StringField("V Mag")
+    est_cnt_rate = StringField("Count Rate")
+    forder_cnt_rate = StringField("1st Order Rate")
 
 class DitherParamForm(FlaskForm):
-    dither_flag  = SelectField("Dither :",  choices=_CHOICE_NNY,)
+    dither_flag  = SelectField("Dither",  choices=_CHOICE_NNY,)
 
 class TimeParamForm(FlaskForm):
     time_ordr = IntegerField("Rank")
@@ -77,11 +77,11 @@ class TimeParamForm(FlaskForm):
     tstop = FieldList(DateTimeField(format=_USINT_DATETIME_FORMAT), label = "Stop")
 
 class SubmitParamForm(FlaskForm):
-    refresh = SubmitField("Refresh :")
-    open_dither = SubmitField("Open Dither :")
-    open_time = SubmitField("Open Time Constraints :")
-    open_roll = SubmitField("Open Roll Constraints :")
-    open_awin = SubmitField("Open Window Constraints :")
+    refresh = SubmitField("Refresh")
+    open_dither = SubmitField("Open Dither")
+    open_time = SubmitField("Open Time Constraints")
+    open_roll = SubmitField("Open Roll Constraints")
+    open_awin = SubmitField("Open Window Constraints")
     submit = SubmitField("Submit")
 
 class OcatParamForm(FlaskForm):
--- cus_app/ocatdatapage/routes.py
+++ cus_app/ocatdatapage/routes.py
@@ -19,6 +19,33 @@ import cus_app.supple.read_ocat_data as rod
 
 _OCAT_DATETIME_FORMAT = "%b %d %Y %I:%M%p"  #: NOTE Ocat dates are recorded without a leading zero. While datetime can process these dates, it never prints without a leading zero
 
+_FORM_BY_CATEGORY = {
+    "gen_param": [
+        "targname",
+        "instrument",
+        "grating",
+        "type",
+        "ra_hms",
+        "dec_dms",
+        "y_det_offset",
+        "z_det_offset",
+        "trans_offset",
+        "focus_offset",
+        "uninterrupt",
+        "extend_src",
+        "obj_flag",
+        "object",
+        "photometry_flag",
+        "vmagnitude",
+        "est_cnt_rate",
+        "forder_cnt_rate",
+    ],
+    "dither_param": [
+        "dither_flag",
+    ],
+    "time_param": ["time_ordr", "window_constraint", "tstart", "tstop"],
+}
+
 
 def verbose_validate_on_submit(form):
     if form.is_submitted():
@@ -30,7 +57,9 @@ def verbose_validate_on_submit(form):
                     validator(form, field)
                 except ValidationError as e:
                     is_valid = False
-                    print(f"Validation failed for {field.name} with {validator.__class__.__name__}: {str(e)}")
+                    print(
+                        f"Validation failed for {field.name} with {validator.__class__.__name__}: {str(e)}"
+                    )
             if field.type == "FormField":
                 for subfield in field:
                     print(f"Checking validators for {subfield.name}:")
@@ -39,9 +68,12 @@ def verbose_validate_on_submit(form):
                             validator(form, subfield)
                             print(f"  {validator.__class__.__name__}: Passed")
                         except ValidationError as e:
-                            print(f"  {validator.__class__.__name__}: Failed - {str(e)}")
+                            print(
+                                f"  {validator.__class__.__name__}: Failed - {str(e)}"
+                            )
         return is_valid
     return False
+
 
 @bp.route("/", methods=["GET", "POST"])
 @bp.route("/<obsid>", methods=["GET", "POST"])
@@ -50,23 +82,14 @@ def index(obsid=None):
     #
     # --- Render Ocat Data In A WTForm
     #
-    ocat_data = rod.read_ocat_data(obsid)
-    #: TODO Write a separation step in which directly ocat_data is formatted into selection specifically used by the form.
-    form_starting_values = {
-        "gen_param": {
-            "obsid": ocat_data.get("osbid"),
-            "targname": ocat_data.get("targname"),
-        },
-        "dither_param": {"dither_flag": ocat_data.get("dither_flag")},
-        "time_param": {
-            "time_ordr": ocat_data.get("time_ordr"),
-            "window_constraint": ocat_data.get("window_constraint"),
-            "tstart": [datetime.strptime(x,_OCAT_DATETIME_FORMAT) for x in ocat_data.get('tstart')],
-            "tstop": [datetime.strptime(x,_OCAT_DATETIME_FORMAT) for x in ocat_data.get('tstop')]
-        },
-    }
-    form = OcatParamForm(request.form, data=form_starting_values)
-    #if form.validate_on_submit():
+    if not request.form:
+        ocat_data = rod.read_ocat_data(obsid)
+        form_starting_values = format_for_form(ocat_data)
+        print(form_starting_values)
+        form = OcatParamForm(data=form_starting_values)
+    else:
+        form = OcatParamForm(request.form)
+    # if form.validate_on_submit():
     if request.method == "POST":
         if verbose_validate_on_submit(form):
             print("Form is valid")
@@ -77,3 +100,27 @@ def index(obsid=None):
             line += f"<p>{key.label} : {key.data}</p>"
         return line
     return render_template("ocatdatapage/index.html", form=form, ocat_data=ocat_data)
+
+
+def format_for_form(ocat_data):
+    form = {}
+    for category, parameter_list in _FORM_BY_CATEGORY.items():
+        form[category] = {}
+        for param in parameter_list:
+            form[category][param] = ocat_data.get(param)
+    #
+    # --- Adjustments for Form
+    #
+    tstart = form["time_param"].get("tstart")
+    if tstart is not None:
+        form["time_param"]["tstart"] = [
+            datetime.strptime(x, _OCAT_DATETIME_FORMAT) for x in tstart
+        ]
+
+    tstop = form["time_param"].get("tstop")
+    if tstop is not None:
+        form["time_param"]["tstop"] = [
+            datetime.strptime(x, _OCAT_DATETIME_FORMAT) for x in tstop
+        ]
+
+    return form
