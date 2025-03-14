b9bbd5634ecf38e5c626ab2f2033f58f55373847
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2025-03-14 13:19:50 -0400

    experimenting with time constraing parameters

--- cus_app/ocatdatapage/forms.py
+++ cus_app/ocatdatapage/forms.py
@@ -5,12 +5,11 @@
 :Last Updated: Mar 13, 2025
 
 """
-
-from re import S
 from flask import request
 from flask_wtf import FlaskForm
-from wtforms import SelectField, StringField, SubmitField, FormField, FieldList
+from wtforms import SelectField, StringField, SubmitField, FormField, FieldList, IntegerField, DateTimeField
 from wtforms.validators import ValidationError, DataRequired, Length
+from datetime import datetime
 
 
 #
@@ -22,44 +21,81 @@ _CHOICE_NNY  = ((None, 'NA'), ('N', 'NO'), ('Y', 'YES'),)
 _CHOICE_CP   = (('Y','CONSTRAINT'),('P','PREFERENCE'),)
 _CHOICE_NNPC = ((None,'NA'),('N','NO'), ('P','PREFERENCE'), ('Y', 'CONSTRAINT'),)
 
+_USINT_DATETIME_FORMAT = "%b-%d-%Y %H:%M"
+
+"""
+**CONCEPT MEMO**: All of the variable and function names within the form classes follow specific FlaskForm criteria in order to
+allow for Jinja Template page generation to input initial data into the Ocat Form using the data argument, as well as validate
+input selections for fields with the validate_<field name> functions. Changing these names will break the form validation unless matched with corresponding
+initial data dictionary keys and field names.
+"""
+
+
 class GeneralParamForm(FlaskForm):
     targname = StringField("Target Name")
 
     choice = ("ACIS-I", "ACIS-S", "HRC-I", "HRC-S")
-    instrument = SelectField("Instrument", choices=[(x, x) for x in choice])
+    instrument = SelectField("Instrument :", choices=[(x, x) for x in choice])
 
     choice = (None, "LETG", "HETG")
-    grating = SelectField("Grating", choices=[(x, x) for x in choice])
+    grating = SelectField("Grating :", choices=[(x, x) for x in choice])
 
     choice = ('GO', 'TOO', 'GTO', 'CAL', 'DDT', 'CAL_ER', 'ARCHIVE', 'CDFS', 'CLP')
-    type = SelectField("Type", choices=[(x, x) for x in choice])
+    type = SelectField("Type :", choices=[(x, x) for x in choice])
 
-    ra_hms = StringField("RA (HMS)")
-    dec_dms = StringField("DEC (DMS)")
+    ra_hms = StringField("RA (HMS) :")
+    dec_dms = StringField("DEC (DMS) :")
 
-    y_det_offset = StringField("Offset Y")
-    z_det_offset = StringField("Offset Z")
-    trans_offset = StringField("Z-Sim")
-    focus_offset = StringField("Sim-Focus")
+    y_det_offset = StringField("Offset Y :")
+    z_det_offset = StringField("Offset Z :")
+    trans_offset = StringField("Z-Sim :")
+    focus_offset = StringField("Sim-Focus :")
 
-    uninterrupt = SelectField("Uninterrupted Obs", choices=_CHOICE_NNPY)
-    extended_src = SelectField("Extended SRC", choices=_CHOICE_NY)
-    obj_flag = SelectField("Solar System Object", choices=[(x, x) for x in ('NO', 'MT', 'SS')])
-    object = SelectField("Object", choices=[(x, x) for x in (None, 'NEW','ASTEROID', 'COMET', 'EARTH', 'JUPITER', 'MARS','MOON', 'NEPTUNE', 'PLUTO', 'SATURN', 'URANUS', 'VENUS')])
-    photometry_flag = SelectField("Photometry", choices=_CHOICE_NNY)
-    vmagnitude = StringField("V Mag")
-    est_cnt_rate = StringField("Count Rate")
-    forder_cnt_rate = StringField("1st Order Rate")
+    uninterrupt = SelectField("Uninterrupted Obs :", choices=_CHOICE_NNPY)
+    extended_src = SelectField("Extended SRC :", choices=_CHOICE_NY)
+    obj_flag = SelectField("Solar System Object :", choices=[(x, x) for x in ('NO', 'MT', 'SS')])
+    object = SelectField("Object :", choices=[(x, x) for x in (None, 'NEW','ASTEROID', 'COMET', 'EARTH', 'JUPITER', 'MARS','MOON', 'NEPTUNE', 'PLUTO', 'SATURN', 'URANUS', 'VENUS')])
+    photometry_flag = SelectField("Photometry :", choices=_CHOICE_NNY)
+    vmagnitude = StringField("V Mag :")
+    est_cnt_rate = StringField("Count Rate :")
+    forder_cnt_rate = StringField("1st Order Rate :")
 
 class DitherParamForm(FlaskForm):
-    dither_flag  = SelectField("Dither",  choices=_CHOICE_NNY,)
+    dither_flag  = SelectField("Dither :",  choices=_CHOICE_NNY,)
+
+class TimeParamForm(FlaskForm):
+    time_ordr = IntegerField("Rank")
+    window_constraint = FieldList(SelectField("Window Constraint",choices=_CHOICE_NNPC))
+    tstart = FieldList(DateTimeField("Start", format=_USINT_DATETIME_FORMAT, validators=[DataRequired()]))
+    tstop = FieldList(DateTimeField("Stop", format=_USINT_DATETIME_FORMAT, validators=[DataRequired()]))
+    
+    all_invalid_time = []
+    #: TODO Why are both validations triggering when only one datetime box is erroring?
+    #: Now always triggering when submitting... try approach with custom validator class instead to feed into the Datetime Field.
+    def validate_tstart(form,field_list):
+        for field in field_list:
+            try:
+                datetime.strptime(field.data, _USINT_DATETIME_FORMAT)
+            except (ValueError, TypeError):
+                error = ValidationError(f'Invalid datetime format in {field.label.text}. Use MMM-DD-YYY HH:MM')
+                form.all_invalid_time.append(error)
+                raise error
+            
+    def validate_tstop(form,field_list):
+        for field in field_list:
+            try:
+                datetime.strptime(field.data, _USINT_DATETIME_FORMAT)
+            except (ValueError, TypeError):
+                error = ValidationError(f'Invalid datetime format in {field.label.text}. Use MMM-DD-YYY HH:MM')
+                form.all_invalid_time.append(error)
+                raise error
 
 class SubmitParamForm(FlaskForm):
-    refresh = SubmitField("Refresh")
-    open_dither = SubmitField("Open Dither")
-    open_time = SubmitField("Open Time Constraints")
-    open_roll = SubmitField("Open Roll Constraints")
-    open_awin = SubmitField("Open Window Constraints")
+    refresh = SubmitField("Refresh :")
+    open_dither = SubmitField("Open Dither :")
+    open_time = SubmitField("Open Time Constraints :")
+    open_roll = SubmitField("Open Roll Constraints :")
+    open_awin = SubmitField("Open Window Constraints :")
     submit = SubmitField("Submit")
 
 class OcatParamForm(FlaskForm):
@@ -69,10 +105,6 @@ class OcatParamForm(FlaskForm):
     Note that this means non-editable information is rendered directly by flask template and not by form method.
     """
     gen_param = FormField(GeneralParamForm)
-
-    #
-    #--- Dither
-    #
     dither_param = FormField(DitherParamForm)
-    
+    time_param = FormField(TimeParamForm)
     submit = FormField(SubmitParamForm)
--- cus_app/ocatdatapage/routes.py
+++ cus_app/ocatdatapage/routes.py
@@ -8,26 +8,44 @@
 
 import os
 import numpy
+from datetime import datetime
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 
-from flask              import current_app, render_template, request
-from cus_app.ocatdatapage   import bp
+from flask import current_app, render_template, request
+from cus_app.ocatdatapage import bp
 from cus_app.ocatdatapage.forms import OcatParamForm
-import cus_app.supple.read_ocat_data         as rod
+import cus_app.supple.read_ocat_data as rod
 
-@bp.route('/',              methods=['GET', 'POST'])
-@bp.route('/<obsid>',       methods=['GET', 'POST'])
-@bp.route('/index/<obsid>', methods=['GET', 'POST'])
+_OCAT_DATETIME_FORMAT = "%b %d %Y %I:%M%p"  #: NOTE Ocat dates are recorded without a leading zero. While datetime can process these dates, it never prints without a leading zero
+
+
+@bp.route("/", methods=["GET", "POST"])
+@bp.route("/<obsid>", methods=["GET", "POST"])
+@bp.route("/index/<obsid>", methods=["GET", "POST"])
 def index(obsid=None):
     #
-    #--- Render Ocat Data In A WTForm
+    # --- Render Ocat Data In A WTForm
     #
     ocat_data = rod.read_ocat_data(obsid)
-    form     = OcatParamForm(request.form, data = ocat_data)
+    #: TODO Write a separation step in which directly ocat_data is formatted into selection specifically used by the form.
+    form_starting_values = {
+        "gen_param": {
+            "obsid": ocat_data.get("osbid"),
+            "targname": ocat_data.get("targname"),
+        },
+        "dither_param": {"dither_flag": ocat_data.get("dither_flag")},
+        "time_param": {
+            "time_ordr": ocat_data.get("time_ordr"),
+            "window_constraint": ocat_data.get("window_constraint"),
+            "tstart": [datetime.strptime(x,_OCAT_DATETIME_FORMAT) for x in ocat_data.get('tstart')],
+            "tstop": [datetime.strptime(x,_OCAT_DATETIME_FORMAT) for x in ocat_data.get('tstop')]
+        },
+    }
+    form = OcatParamForm(request.form, data=form_starting_values)
+    #if form.validate_on_submit():
     if request.method == 'POST' and form.submit.submit:
-        line = ''
+        line = ""
         for key in form:
             line += f"<p>{key.label} : {key.data}</p>"
         return line
-    return render_template('ocatdatapage/index.html', form=form, ocat_data=ocat_data)
-
+    return render_template("ocatdatapage/index.html", form=form, ocat_data=ocat_data)
