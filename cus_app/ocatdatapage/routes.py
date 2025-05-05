44ab48c41c8c0f42673dd4d686fdeae4b8a50c04
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2025-05-05 18:00:10 -0400

    error processing in dev

--- cus_app/ocatdatapage/routes.py
+++ cus_app/ocatdatapage/routes.py
@@ -24,6 +24,7 @@ import os
 import re
 import json
 from datetime import datetime
+import traceback
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 from wtforms.validators import ValidationError
 
@@ -103,22 +104,22 @@ def confirm(obsid=None):
             return redirect(url_for('ocatdatapage.index', obsid=obsid))
         elif form.finalize.data:
             #: Write changes to the database files
-            #try:
-            rev = dbi.construct_revision(obsid,ocat_data,ocat_form_dict.get("submit_choice"))
-            db.session.add(rev)
-            sign = dbi.construct_signoff(rev,req_dict)
-            db.session.add(sign)
-            reqs = dbi.construct_requests(rev, req_dict)
-            for req in reqs:
-                db.session.add(req)
-            orgs = dbi.construct_originals(rev, org_dict)
-            for org in orgs:
-                db.session.add(org)
-            #except:  # noqa: E722
+            try:
+                rev = dbi.construct_revision(obsid,ocat_data,ocat_form_dict.get("submit_choice"))
+                db.session.add(rev)
+                sign = dbi.construct_signoff(rev,req_dict)
+                db.session.add(sign)
+                reqs = dbi.construct_requests(rev, req_dict)
+                for req in reqs:
+                    db.session.add(req)
+                orgs = dbi.construct_originals(rev, org_dict)
+                for org in orgs:
+                    db.session.add(org)
+            except:  # noqa: E722
                 #: In the event of an error, roll back the database session to avoid commits instilled by the server-side cookies
                 #: TODO. Do we still clear the session cookies if the database injection failed? I'd assume not...
-                #db.session.rollback()
-                #abort(500)
+                db.session.rollback()
+                traceback.print_exc() #: TODO replace with abort(500)
             return redirect(url_for('ocatdatapage.finalize', obsids=[int(obsid)]+multi_obsid))
     return render_template('ocatdatapage/confirm.html',
                             form = form,
