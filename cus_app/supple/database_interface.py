a0ac0966ed9227493a47caafe2dd9a3dec790d17
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2025-05-06 17:19:47 -0400

    database interface functions for fetching revisions and signoffs.

--- cus_app/supple/database_interface.py
+++ cus_app/supple/database_interface.py
@@ -17,12 +17,12 @@ import sys
 import os
 from datetime import datetime
 import json
-from sqlalchemy import select, insert
+from sqlalchemy import select, insert, desc
 from sqlalchemy.orm.exc import NoResultFound
 from cus_app import db
 from cus_app.models     import register_user, User, Revision, Signoff, Parameter, Request, Original
 from flask_login    import current_user
-from cus_app.supple.helper_functions import coerce_json
+from cus_app.supple.helper_functions import coerce_json, DATETIME_FORMATS
 
 stat_dir =  os.path.join(os.path.dirname(os.path.abspath(__file__)),'..', 'static')
 with open(os.path.join(stat_dir, 'parameter_selections.json')) as f:
@@ -50,18 +50,10 @@ def construct_signoff(rev_obj, req_dict={}):
     signoff time is the epoch time of when the signoff was made.
     The signoff itself lists the user id.
     """
-    curr_epoch = int(datetime.now().timestamp())
+    
     if rev_obj.kind in ('asis', 'remove'):
         #:Adding or removing from the approved list. Auto signoff with Usint revision performing user.
-        signoff = Signoff(revision = rev_obj,
-                            general_status = 'Not Required',
-                            acis_status = 'Not Required',
-                            acis_si_status = 'Not Required',
-                            hrc_si_status = 'Not Required',
-                            usint_status = 'Signed',
-                            usint_signoff_id = rev_obj.user_id,
-                            usint_time = curr_epoch
-                    )
+        signoff = construct_auto_signoff(rev_obj)
     elif rev_obj.kind == 'clone':
         #: Needs ArcOps and Usint Signoff
         signoff = Signoff(revision = rev_obj,
@@ -81,6 +73,22 @@ def construct_signoff(rev_obj, req_dict={}):
                           hrc_si_status = hrc_si,
                           usint_status = 'Pending'
         )
+    return signoff
+
+def construct_auto_signoff(rev_obj):
+    """
+    Automatically fill a usint signoff ORM without other signoffs.
+    """
+    curr_epoch = int(datetime.now().timestamp())
+    signoff = Signoff(revision = rev_obj,
+                            general_status = 'Not Required',
+                            acis_status = 'Not Required',
+                            acis_si_status = 'Not Required',
+                            hrc_si_status = 'Not Required',
+                            usint_status = 'Signed',
+                            usint_signoff_id = rev_obj.user_id,
+                            usint_time = curr_epoch
+                    )
     return signoff
 
 def construct_requests(rev_obj, req_dict):
@@ -145,6 +153,52 @@ def pull_param(param):
         #: Return same error with more specifics
         raise NoResultFound(f"No result for '{param}' parameter search in table.")
 
+    return result
+
+def pull_revision(**kwargs):
+    """
+    Fetch list of recent revisions based on kwarg criteria
+    """
+    def _to_epoch(time):
+        if time is None:
+            return None
+        elif isinstance(time,(int, float)):
+            return time
+        elif isinstance(time, datetime):
+            return time.timestamp()
+        elif isinstance(time,str):
+            x = time.replace('::', ':')
+            x = x.split('.')[0]
+            for format in DATETIME_FORMATS:
+                try:
+                    return datetime.strptime(x,format).timestamp()
+                except ValueError:
+                    pass
+    #
+    # --- Fetch by recent number
+    #
+    last = kwargs.get('last')
+    if last is not None:
+        return db.session.execute(select(Revision).order_by(desc(Revision.id)).limit(last)).all()
+
+    #
+    # --- Fetch by time interval
+    #
+    before = _to_epoch(kwargs.get('before'))
+    after = _to_epoch(kwargs.get('after'))
+    if before is None and after is not None:
+        return db.session.execute(select(Revision).where(Revision.time >= after).order_by(desc(Revision.id))).all()
+    elif before is not None and after is None:
+        return db.session.execute(select(Revision).where(Revision.time <= before).order_by(desc(Revision.id))).all()
+    elif before is not None and after is not None:
+        return db.session.execute(select(Revision).where(Revision.time <= before).where(Revision.time >= after).order_by(desc(Revision.id))).all()        
+
+    
+def pull_signoff(rev_obj):
+    """
+    Fetch signoff matching provides revision
+    """
+    result = db.session.execute(select(Signoff).where(Signoff.revision_id == rev_obj.id)).scalar_one()
     return result
 
 def find_next_rev_no(obsid):
