399175adf6e0e45e3a5adca743f73088c3546930
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2025-05-16 13:16:50 -0400

    form for removing a submission.

---
+++ cus_app/rm_submission/forms.py
@@ -0,0 +1,17 @@
+"""
+**rm_submission/forms.py**: Flask WTForm of the Remove Submission Page
+
+:Author: W. Aaron (william.aaron@cfa.harvard.edu)
+:Last Updated: May 015, 2025
+
+"""
+from flask_wtf import FlaskForm
+from wtforms import SubmitField
+
+class RemoveRow(FlaskForm):
+    revision = SubmitField("Remove")
+    general = SubmitField("Remove")
+    acis = SubmitField("Remove")
+    acis_si = SubmitField("Remove")
+    hrc_si = SubmitField("Remove")
+    usint = SubmitField("Remove")
